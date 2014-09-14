e8aa8ab8625382e0f4aa55b2ff0314b1b345a760
Filip Dolník <fdolnik@volny.cz>
2014-09-14 20:52:48 +0200

    Add section "What is it we do?".

---
+++ js/language.js
@@ -0,0 +1,7 @@
+$(window).scroll(function () {
+    if ($("#content").offset().top - $(document).scrollTop() < 0) {
+        $("#language").css("position", "fixed");
+    } else {
+        $("#language").css("position", "absolute");
+    }
+});
