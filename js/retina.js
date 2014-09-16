e17427c95bb2ecfec727a52f6e6f29bd5f47d632
Filip Dolník <fdolnik@volny.cz>
2014-09-16 21:46:52 +0200

    Fix retina support.

--- js/retina.js
+++ js/retina.js
@@ -1,21 +1,15 @@
 $(document).ready(function() {
-    $("img").each(function() {
-        if (window.devicePixelRatio > 1) {
+    if (window.devicePixelRatio > 1) {
+        $("img").each(function() {
             path = $(this).attr("src").replace("img", "img2x");
-        } else {
-            path = $(this).attr("src").replace("img", "img1x");
-        }
-        $(this).attr("src", path); 
-    });
-    $("*").each(function() {
-        image = $(this).css("background-image");
-        if (image !== "none") {
-            if (window.devicePixelRatio > 1) {
+            $(this).attr("src", path); 
+        });
+        $("*").each(function() {
+            image = $(this).css("background-image");
+            if (image !== "none") {
                 image = image.replace("img", "img2x");
-            } else {
-                image = image.replace("img", "img1x");
+                $(this).css("background-image", image);
             }
-            $(this).css("background-image", image);
-        }
-    });
+        });
+    }
 });
