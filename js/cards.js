ca3bdcf4b7c9e34fb9854ca4f8bf2414839ea85d
Filip Dolník <fdolnik@volny.cz>
2014-09-20 21:37:10 +0200

    Add mobile version.

--- js/cards.js
+++ js/cards.js
@@ -1,15 +1,11 @@
+bigScreen = null;
+interval = null;
+
 $(document).ready(function() {
-    currentCardIcon = null;
-    setRandomCard();
-    interval = setInterval(function() {
-        setRandomCard();
-    }, 3000);
-    
-    $("#whatWeDid .card img").click(function() {
-        clearInterval(interval);
-        setCard(this);
-    });
+    screenSizeChange();
 });
+
+$(window).resize(screenSizeChange);
 
 function setCard(cardIcon) {
     fadeDuration = 250;
@@ -17,18 +13,18 @@ function setCard(cardIcon) {
         return;
     }
     if (currentCardIcon !== null) {
-        $(currentCardIcon).parent().css("z-index", "3");
+        $(currentCardIcon).parentsUntil("#cards", ".card").css("z-index", "3");
         $(currentCardIcon).fadeTo(fadeDuration, 0.3, function() {
             $(this).css("opacity", "");
         });
-        $(currentCardIcon).parent().find(".cardInfo").fadeTo(fadeDuration / 0.7, 0, function() {
-            $(this).parent().css("z-index", "1");
+        $(currentCardIcon).parentsUntil("#cards", ".card").find(".cardInfo").fadeTo(fadeDuration / 0.7, 0, function() {
+            $(this).parentsUntil("#cards", ".card").css("z-index", "1");
         });
     }
     currentCardIcon = cardIcon;
-    $(currentCardIcon).parent().css("z-index", "4");
+    $(currentCardIcon).parentsUntil("#cards", ".card").css("z-index", "4");
     $(currentCardIcon).fadeTo(fadeDuration, 1);
-    $(currentCardIcon).parent().find(".cardInfo").fadeTo(fadeDuration / 0.7, 1);
+    $(currentCardIcon).parentsUntil("#cards", ".card").find(".cardInfo").fadeTo(fadeDuration / 0.7, 1);
 }
 
 function setRandomCard() {
@@ -42,4 +38,57 @@ function getRandomIndex(max, oldIndex) {
         return getRandomIndex(max, oldIndex);
     }
     return newIndex;
+}
+
+function resetCards() {
+    clearInterval(interval);
+    $("#whatWeDid .card img").click(null);
+    $("#whatWeDid #showMore").click(null);
+    $("#whatWeDid #showLess").click(null);
+    $("#whatWeDid .card img").each(function() {
+        $(this).stop();
+        $(this).css("opacity", "");
+        $(this).parentsUntil("#cards", ".card").find(".cardInfo").stop();
+        $(this).parentsUntil("#cards", ".card").css("z-index", "");
+        $(this).parentsUntil("#cards", ".card").find(".cardInfo").css("opacity", "");
+    });
+    $("#whatWeDid #moreCards").css("display", "");
+    $("#whatWeDid #showLess").css("display", "");
+    $("#whatWeDid #showMore").css("display", "");
+}
+
+function checkScreenSize() {
+    return window.matchMedia("screen and (min-width: 1100px)").matches;
+}
+
+function screenSizeChange() {
+    oldScreen = bigScreen;
+    bigScreen = checkScreenSize();
+    if (oldScreen === bigScreen) {
+        return;
+    }
+    resetCards();
+    if (bigScreen) {
+        currentCardIcon = null;
+        setRandomCard();
+        interval = setInterval(function() {
+            setRandomCard();
+        }, 3000);
+
+        $("#whatWeDid .card img").click(function() {
+            clearInterval(interval);
+            setCard(this);
+        });
+    } else {
+        $("#whatWeDid #showMore").click(function() {
+            $("#whatWeDid #moreCards").slideDown("slow");
+            $("#whatWeDid #showMore").css("display", "none");
+            $("#whatWeDid #showLess").css("display", "inline-block");
+        });
+        $("#whatWeDid #showLess").click(function() {
+            $("#whatWeDid #moreCards").slideUp("slow");
+            $("#whatWeDid #showLess").css("display", "none");
+            $("#whatWeDid #showMore").css("display", "inline-block");
+        });
+    }
 }
