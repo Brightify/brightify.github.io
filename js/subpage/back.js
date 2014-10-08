7766277e8f6bbf498e331b1b8f6943b28d56f209
Filip Dolník <fdolnik@volny.cz>
2014-10-08 17:38:27 +0200

    Fix bug introduced in last commit.

--- js/subpage/back.js
+++ js/subpage/back.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#back").click(function(event) {
-        if (document.referrer.indexOf("Brightify") > -1) {
+        if (document.referrer.indexOf("brightify") > -1) {
             window.history.back();
             event.preventDefault();
         }  
