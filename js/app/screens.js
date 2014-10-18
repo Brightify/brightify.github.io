b2b483c883b2aa7e26e67b0f66a8dc0f13591af0
Filip Dolník <fdolnik@volny.cz>
2014-10-18 17:28:24 +0200

    Adjust screens animation.

--- js/app/screens.js
+++ js/app/screens.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    screensOffset = 0;
+    screensWidth = 0;
     onWidthChange();
     $("#content .platform div").click(function() {
         setScreensVisible($.inArray(this, $("#content .platform div")), true);
@@ -8,24 +10,33 @@ $(document).ready(function() {
 $(window).resize(onWidthChange);
 
 function onWidthChange() {
-    var screenWidth = getScreenWidth();
-    var width = $("#content .screens").size() * screenWidth;
+    var windowWidth = getWindowWidth();
+    var newScreensWidth = $("#content .screens").width();
+    var width = $("#content .screens").size() * windowWidth;
     $("#content .allScreens").width(width);
-    var margin = screenWidth - $("#content .screens").width();
+    var margin = windowWidth - newScreensWidth;
     $("#content .screens").css("margin-right", margin);
+    if (newScreensWidth !== screensWidth) {
+        screensWidth = newScreensWidth;
+        $("#content .screens *").css("left", "");
+        screensOffset = 0;
+    }
     setScreensVisible(getSelectedIndex(), false);
 }
 
 function setScreensVisible(index, animate) {
-    var screenWidth = getScreenWidth();
+    var windowWidth = getWindowWidth();
     $("#content .platform div").removeClass("selected");
     $($("#content .platform div")[index]).addClass("selected");
-    var left = screenWidth * index * -1 + (screenWidth - $("#content .screens").width()) / 2;
+    var left = windowWidth * index * -1 + (windowWidth - screensWidth) / 2;
+    left -= screensOffset;
     if (animate) {
-        $("#content .allScreens").animate({left: left});
+        var duration = Math.abs(Math.ceil(left / windowWidth)) * 400;
+        animateScreens(left, duration);
     } else {
-        $("#content .allScreens").css("left", left);
+        moveScreens(left);
     }
+    screensOffset += left;
 }
 
 function getSelectedIndex() {
@@ -39,6 +50,27 @@ function getSelectedIndex() {
     return result;
 }
 
-function getScreenWidth() {
+function getWindowWidth() {
     return Math.max(530, $(window).width());
+}
+
+function animateScreens(left, duration) {
+    var interval = 50;
+    if (left > 0) {
+        animateOneScreen(left, 3, duration);
+        setTimeout(function() {animateOneScreen(left, 2, duration);}, interval);
+        setTimeout(function() {animateOneScreen(left, 1, duration);}, interval * 2);
+    } else {
+        animateOneScreen(left, 1, duration);
+        setTimeout(function() {animateOneScreen(left, 2, duration);}, interval);
+        setTimeout(function() {animateOneScreen(left, 3, duration);}, interval * 2);
+    }
+}
+
+function animateOneScreen(left, index, duration) {
+    $("#content .screens *:nth-of-type("+ index +")").animate({left: "+=" + left + "px"}, duration);
+}
+
+function moveScreens(left) {
+    $("#content .screens *").css("left", "+=" + left + "px");
 }
